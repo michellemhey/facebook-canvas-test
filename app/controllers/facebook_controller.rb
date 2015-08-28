d9b74103f33f2d988ae0376e23344219e23c8e82
Michelle Hey <michelle.hey@metova.com>
2015-08-28 13:28:24 -0500

    allow facebook iframe

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,12 +1,19 @@
 class FacebookController < ApplicationController
   skip_before_filter :verify_authenticity_token
   skip_before_filter :detect_facebook_post!
+  after_action :allow_facebook_iframe
 
-  def channel
-    cache_expire = 1.year
-    response.headers["Pragma"] = "public"
-    response.headers["Cache-Control"] = "max-age=#{cache_expire.to_i}"
-    response.headers["Expires"] = (Time.now + cache_expire).strftime("%d %m %Y %H:%I:%S %Z")
-    render :layout => false, :inline => "<script src='//connect.facebook.net/en_US/all.js'></script>"
+  # def channel
+  #   cache_expire = 1.year
+  #   response.headers["Pragma"] = "public"
+  #   response.headers["Cache-Control"] = "max-age=#{cache_expire.to_i}"
+  #   response.headers["Expires"] = (Time.now + cache_expire).strftime("%d %m %Y %H:%I:%S %Z")
+  #   render :layout => false, :inline => "<script src='//connect.facebook.net/en_US/all.js'></script>"
+  # end
+
+  private
+
+  def allow_facebook_iframe
+    response.headers['X-Frame-Options'] = 'ALLOW-FROM https://apps.facebook.com'
   end
 end
